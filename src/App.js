c82e4200c738612e52869232eb0beeb6457f0abc
omar.khan1 <omar.khan1@fdmgroup.com>
2022-02-04 15:26:14 -0500

    Created the initial buttons and fields

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
+function mockAPIcall() {
+	const requestOptions = {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ title: 'React POST Request Example' }),
+	};
+	fetch('https://reqres.in/api/posts', requestOptions);
+
+  
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="Project-header">
+			<input
+				type="email"
+				class="form-control"
+				id="exampleInputEmail1"
+				aria-describedby="emailHelp"
+				placeholder="Enter email"
+			/>
+			<input
+				type="password"
+				class="form-control"
+				id="exampleInputPassword1"
+				placeholder="Password"
+			/>
+			<br />
+			<button type="button" class="btn btn-primary" onClick={mockAPIcall()}>
+				Login
+			</button>
+		</div>
+	);
 }
 
 export default App;
